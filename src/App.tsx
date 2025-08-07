7c5cc0488f6c29c13f4580135e563073611115b4
Udo Quattelbaum <u.quattelbaum@outlook.com>
2025-08-07 23:27:50 +0200

    Noch ein Deploy-Versuch

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,34 @@ export default function App() {
     <div>
       {/* Navigation */}
       <nav className="bg-gray-100 p-4 flex gap-4 text-sm border-b">
-        <NavLink to="/mitglieder" className={linkClass}>Mitglieder</NavLink>
-        <NavLink to="/abteilungen" className={linkClass}>Abteilungen</NavLink>
-        <NavLink to="/gruppen" className={linkClass}>Gruppen</NavLink>
+        <NavLink to="/mitglieder" className={linkClass}>
+          Mitglieder
+        </NavLink>
+        <NavLink to="/abteilungen" className={linkClass}>
+          Abteilungen
+        </NavLink>
+        <NavLink to="/gruppen" className={linkClass}>
+          Gruppen
+        </NavLink>
       </nav>
 
       {/* Inhalt */}
       <div className="p-6">
         <Routes>
-          <Route path="/" element={<Navigate to="/mitglieder" />} /> {/* Weiterleitung */}
+          <Route path="/" element={<Navigate to="/mitglieder" />} />{" "}
+          {/* Weiterleitung */}
           <Route path="/mitglieder" element={<Mitglieder />} />
-          <Route path="/mitglieder/neu" element={<MitgliedFormular />} /> {/* ✨ Neu */}
+          <Route
+            path="/mitglieder/neu"
+            element={
+              <MitgliedFormular
+                onDone={() => {
+                  window.history.back();
+                }}
+              />
+            }
+          />{" "}
+          {/* ✨ Neu */}
           <Route path="/abteilungen" element={<Abteilungen />} />
           <Route path="/gruppen" element={<Gruppen />} />
         </Routes>
